58a3ec65bb458cd4f34aa087b0ee1fe6be31cf4a
Rhayra Madoglio <111394002+Rhayragirl@users.noreply.github.com>
2024-04-15 17:11:20 -0300

    Jornada Tech Ebac
    Desenvolvendo uma IA com leaut de jogo.

---
+++ RNA.js
@@ -0,0 +1,95 @@
+function randomRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+  
+  
+  function lerp(a, b, t) {
+    return a + (b - a) * t;
+  }
+  
+  class Neuron {
+    constructor(inputs) {
+  
+      this.bias = randomRange(-1, 1);
+      
+      this.weightList = new Array(inputs)
+        .fill()
+        .map(() => randomRange(-1, 1));
+    };
+  
+    g(signalList = []) {
+      let u = 0;
+
+      for (let i = 0; i < this.weightList.length; i++) {
+        u += signalList[i] * this.weightList[i];
+      }
+  
+      if (Math.tanh(u) > this.bias) return 1;
+      else return 0; 
+    }
+  
+    mutate(rate = 1) {
+      this.weightList = this.weightList.map((w) => {
+
+        return lerp(w, randomRange(-1, 1), rate);
+      });
+    
+      this.bias = lerp(this.bias, randomRange(-1, 1), rate);
+    }
+  }
+
+  class  RNA  {
+    construtor ( inputCount = 1, levelList =[]){
+      this.score = 0;
+
+      this.levelList = levelList.map((l, 1) = > {
+          const inputSize = i === 0 ? inputCount : levelList (i - 1)
+
+          return new Array (l).fill().map(() => new Neuron(inputSize));
+      });
+    }
+
+    compute (list = []){
+      for (let i = 0; i < this.levelList.length; i++) {
+        const tempList = []
+
+        for (const neuron of this.levelList[i]){
+          if (list.length l== neuron.weight.length) throw new Error ("Entrada invalida");
+          tempList.push(neuron.g(list))
+        }
+        list = tempList;
+
+        return list;
+      }
+    }
+
+    mutate(rate = l);{
+      for (const level of this.levalList){
+        for (const neuron of leval) neuron.mutate(rate)
+      }
+    }
+   load (rna);[
+    if (!rna) return;
+    try {
+      this.levelList = rna.map((neuronlist)=> {
+        return neuronlist.map((neuron) => {
+          const n = new Neuron ();
+          n.bias = neuron.biasn
+          n.weightList = neuron.weightList;
+
+          return n;
+        });
+      });
+
+    } catch (e) {
+      return;
+    }
+    save();{
+      return this.levelList;
+    }
+
+    export default RNA;
+  ]
+
+
+   }
